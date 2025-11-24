87703829cb280a4ee9d4ef39c952b8ec426c6300
aristokragus <agusdindin842@gmail.com>
2025-11-25 00:18:24 +0800

    menambah icon dan memperbaiki inputan

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const activeList = document.getElementById('activeList');
+const completedList = document.getElementById('completedList');
+const countDisplay = document.getElementById('countDisplay');
 
 function triggerAddTask() {
     const text = taskInput.value.trim();
@@ -21,6 +23,38 @@ addTaskBtn.addEventListener('click', function () {
 
 function createTaskElement(text) {
     const li = document.createElement('li');
-    li.innerHTML = `<span class="task-text">${text}</span>`; 
+    li.innerHTML = `
+        <span class="task-text">${text}</span>
+        <div class="actions">
+            ${generateButtons('active')}
+        </div>
+    `;
     activeList.appendChild(li);
+}
+
+function generateButtons(status) {
+    if (status === 'active') {
+        return `
+        <div class="action-btn done-btn" onclick="toggleStatus(this)"><i class="fas fa-check"></i></div>
+        <div class="action-btn delete-btn" onclick="deleteTask(this)"><i class="fas fa-trash"></i></div>`;
+    }
+}
+
+function deleteTask(element) {
+    if(confirm("Hapus tugas?")){
+        element.closest('li').remove();
+    }
+}
+
+function toggleStatus(element) {
+    const li = element.closest('li');
+    li.classList.toggle('completed');
+    
+    if (li.classList.contains('completed')) {
+        completedList.prepend(li);
+        li.querySelector('.actions').innerHTML = generateButtons('completed');
+    } else {
+        activeList.prepend(li);
+        li.querySelector('.actions').innerHTML = generateButtons('active');
+    }
 }
